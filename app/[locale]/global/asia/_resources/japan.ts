1ce1c6931d61b93b3d93ca3935b2968a2cc82c2d
creditcityIvar <po.chajm@creditncity.com>
2025-11-23 16:42:09 +0900

    일본 아이콘 추가

--- app/[locale]/global/asia/_resources/japan.ts
+++ app/[locale]/global/asia/_resources/japan.ts
@@ -7,7 +7,7 @@ export const japanResourcesEn: CountryResources = {
       description:
         "A Bitcoin hub space in Tokyo that serves as the center for Bitcoin community activities and education",
       url: "https://tokyobitcoin.space/",
-      logo: "https://placehold.co/80x80?text=TBB",
+      logo: "https://pbs.twimg.com/profile_images/1926880030631636992/wkOL0Hux_400x400.jpg",
     },
   ],
   events: [
@@ -16,7 +16,7 @@ export const japanResourcesEn: CountryResources = {
       description:
         "An organization that hosts major Bitcoin-related events and conferences in Japan and shares information",
       url: "https://btc-jpn.com/",
-      logo: "https://placehold.co/80x80?text=BTCJPN",
+      logo: "https://pbs.twimg.com/profile_images/1949719771219632128/MMqgW-UJ_400x400.jpg",
     },
   ],
   meetups: [
@@ -25,14 +25,14 @@ export const japanResourcesEn: CountryResources = {
       description:
         "Provides regular meetups where you can view various Bitcoin-related meetup information held in Tokyo at a glance",
       url: "https://tokyobitcoin.space/#event-calender",
-      logo: "https://placehold.co/80x80?text=TBB",
+      logo: "https://pbs.twimg.com/profile_images/1926880030631636992/wkOL0Hux_400x400.jpg ",
     },
     {
       name: "Givers",
       description:
         "A group that quickly shares Bitcoin community meetup news and information through active social media",
       url: "https://x.com/_sophia_givers",
-      logo: "https://placehold.co/80x80?text=Givers",
+      logo: "https://pbs.twimg.com/profile_images/1975398273306292224/_LEhK1gp_400x400.jpg",
     },
   ],
   mining: [
@@ -41,7 +41,7 @@ export const japanResourcesEn: CountryResources = {
       description:
         "A company that provides professional solutions and information related to Bitcoin mining",
       url: "https://rai-lai.com/en",
-      logo: "https://placehold.co/80x80?text=RaiLai",
+      logo: "https://pbs.twimg.com/profile_images/1844229895079276544/ipVVqgXC_400x400.jpg",
     },
   ],
   charity: [
@@ -50,28 +50,28 @@ export const japanResourcesEn: CountryResources = {
       description:
         "A venture capital that invests in Bitcoin and Lightning Network-related startups and supports ecosystem growth",
       url: "https://lostinbitcoin.jp/",
-      logo: "https://placehold.co/80x80?text=LiB",
+      logo: "https://pbs.twimg.com/profile_images/1501031201502818304/rMs9IbMJ_400x400.jpg",
     },
     {
       name: "Diamond Hands",
       description:
         "An organization focused on Bitcoin education and community building, conducting various activities for Bitcoin supporters",
       url: "https://www.diamondhands.community/en",
-      logo: "https://placehold.co/80x80?text=DH",
+      logo: "https://pbs.twimg.com/profile_images/1589861221469077507/bJGbcrc8_400x400.jpg",
     },
     {
       name: "ANAP Holdings",
       description:
         "A platform that shares corporate and investment information related to Bitcoin through social media",
       url: "https://x.com/ANAP_3189_T",
-      logo: "https://placehold.co/80x80?text=ANAP",
+      logo: "https://pbs.twimg.com/profile_images/1980521481210568704/7JLaNj1n_400x400.jpg",
     },
     {
       name: "KYGO",
       description:
         "An organization or company contributing to Bitcoin-related technology development and business expansion, supporting global activities",
       url: "https://en.kygo3.com/",
-      logo: "https://placehold.co/80x80?text=KYGO",
+      logo: "https://en.kygo3.com/wp-content/uploads/2022/10/HEADER_LOGO.png?1763883485",
     },
   ],
 };
@@ -83,7 +83,7 @@ export const japanResourcesKo: CountryResources = {
       description:
         "도쿄의 비트코인 허브 공간으로, 비트코인 커뮤니티 활동 및 교육의 중심 역할을 하는 곳입니다",
       url: "https://tokyobitcoin.space/",
-      logo: "https://placehold.co/80x80?text=TBB",
+      logo: "https://pbs.twimg.com/profile_images/1926880030631636992/wkOL0Hux_400x400.jpg",
     },
   ],
   events: [
@@ -92,7 +92,7 @@ export const japanResourcesKo: CountryResources = {
       description:
         "일본 내에서 비트코인 관련 주요 이벤트 및 컨퍼런스를 조직하고 정보를 공유하는 단체입니다",
       url: "https://btc-jpn.com/",
-      logo: "https://placehold.co/80x80?text=BTCJPN",
+      logo: "https://pbs.twimg.com/profile_images/1949719771219632128/MMqgW-UJ_400x400.jpg",
     },
   ],
   meetups: [
@@ -101,14 +101,14 @@ export const japanResourcesKo: CountryResources = {
       description:
         "정기적인 모임을 제공하여, 도쿄에서 열리는 다양한 비트코인 관련 밋업 정보를 한눈에 확인할 수 있습니다",
       url: "https://tokyobitcoin.space/#event-calender",
-      logo: "https://placehold.co/80x80?text=TBB",
+      logo: "https://pbs.twimg.com/profile_images/1926880030631636992/wkOL0Hux_400x400.jpg",
     },
     {
       name: "Givers",
       description:
         "활발한 소셜 미디어를 통해 비트코인 커뮤니티의 모임 소식 및 정보를 빠르게 공유하는 그룹입니다",
       url: "https://x.com/_sophia_givers",
-      logo: "https://placehold.co/80x80?text=Givers",
+      logo: "https://pbs.twimg.com/profile_images/1975398273306292224/_LEhK1gp_400x400.jpg",
     },
   ],
   mining: [
@@ -117,7 +117,7 @@ export const japanResourcesKo: CountryResources = {
       description:
         "비트코인 마이닝(채굴)과 관련된 전문적인 솔루션 및 정보를 제공하는 회사입니다",
       url: "https://rai-lai.com/en",
-      logo: "https://placehold.co/80x80?text=RaiLai",
+      logo: "https://pbs.twimg.com/profile_images/1844229895079276544/ipVVqgXC_400x400.jpg",
     },
   ],
   charity: [
@@ -126,28 +126,28 @@ export const japanResourcesKo: CountryResources = {
       description:
         "비트코인 및 Lightning Network 관련 스타트업에 투자하고 생태계 성장을 지원하는 벤처 캐피털입니다",
       url: "https://lostinbitcoin.jp/",
-      logo: "https://placehold.co/80x80?text=LiB",
+      logo: "https://pbs.twimg.com/profile_images/1501031201502818304/rMs9IbMJ_400x400.jpg",
     },
     {
       name: "Diamond Hands",
       description:
         "비트코인 교육과 커뮤니티 구축에 중점을 두며, 비트코인 지지자들을 위한 다양한 활동을 펼치는 단체입니다",
       url: "https://www.diamondhands.community/en",
-      logo: "https://placehold.co/80x80?text=DH",
+      logo: "https://pbs.twimg.com/profile_images/1589861221469077507/bJGbcrc8_400x400.jpg",
     },
     {
       name: "ANAP Holdings",
       description:
         "소셜 미디어를 통해 비트코인과 관련된 기업 및 투자 정보를 공유하는 곳으로 보입니다",
       url: "https://x.com/ANAP_3189_T",
-      logo: "https://placehold.co/80x80?text=ANAP",
+      logo: "https://pbs.twimg.com/profile_images/1980521481210568704/7JLaNj1n_400x400.jpg",
     },
     {
       name: "KYGO",
       description:
         "비트코인 관련 기술 개발 및 사업 확장에 기여하는 단체 또는 기업으로, 글로벌 활동을 지원합니다",
       url: "https://en.kygo3.com/",
-      logo: "https://placehold.co/80x80?text=KYGO",
+      logo: "https://en.kygo3.com/wp-content/uploads/2022/10/HEADER_LOGO.png?1763883485",
     },
   ],
 };
