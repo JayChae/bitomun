ba51d07d045a1859356be657e79abe897a72f787
creditcityIvar <po.chajm@creditncity.com>
2025-11-23 16:56:44 +0900

    호주 아이콘 추가

--- app/[locale]/global/oceania/_resources/australia.ts
+++ app/[locale]/global/oceania/_resources/australia.ts
@@ -7,14 +7,14 @@ export const australiaResourcesEn: CountryResources = {
       description:
         "The biggest decentralized Bitcoin-Only conference - It's Free!",
       url: "https://bitcoinbushbash.info/",
-      logo: "https://placehold.co/80x80?text=BBB",
+      logo: "https://pbs.twimg.com/profile_images/1324113013301915649/1xyPdFF-_400x400.jpg",
     },
     {
       name: "Bitcoin Alive",
       description:
         "The biggest Bitcoin-Only conference with world class speakers",
       url: "https://bitcoinalive.io/",
-      logo: "https://placehold.co/80x80?text=BA",
+      logo: "https://pbs.twimg.com/profile_images/1594342404336885761/eBkrumv4_400x400.png",
     },
   ],
   meetups: [
@@ -23,14 +23,14 @@ export const australiaResourcesEn: CountryResources = {
       description:
         "The Bitcoin meetup run by Bitcoiners, for Bitcoiners old and new",
       url: "https://www.meetup.com/melbourne-bitcoin-only/",
-      logo: "https://placehold.co/80x80?text=BM",
+      logo: "https://cdn5.telesco.pe/file/n6ek0cYGVHE0f-j27soQ4nUc-UiVoSNMLubWHUjY5UdXBzZJhT8tRweNkUidY8-ktoosKciVd1C-cZL2YOeDThyRmoA2TCnt06ptgoatvB85AukUjiCgispb5VynBSefPZYlP4S5tVYU0Fxk9gn6pRp06ntfW0emjdwKTIBaf8VU3UmY_KVBcDpAnpVZyiIBBdOrUiPXsxh2hQ2b7r2CgLBbwGrJ_nj29Q7c4KCt4QXRplMliGA9KKFuw_hmLEzHCbVzPyF9nrpJpPtJzLZ4FUYAXXJ6dyhFLFFwSyjJY5o-hud7nAx3bynmUqetCqY5W7YLh1bqD_fwRyxkYGnaFA.jpg",
     },
     {
       name: "Bitcoin Sydney",
       description:
         "Discussion about the only Blockchain that matters - Bitcoin",
       url: "https://www.meetup.com/Bitcoin_Sydney/",
-      logo: "https://placehold.co/80x80?text=BS",
+      logo: "https://cdn5.telesco.pe/file/grr-TtL_1OIOA1DfjIMlRtYyl9zuBP5yjne0T0lYOFzQ6uJeaUV_UMa4DBuXkvCKsdFvNwZatDkqEaDqUi8IwssF2fSMWPNiWNW4L5FF6XGumjMF5LKxE0zLXwSU28K1W0k5D9ZcpjAu2C3hfy7bfm8YlCAJqm2NwAPxOxolgPFzqO4IZ3-m3SmfBAj95wFFJ72PgxfjetgpQQJjFEHFaBVR5TJfhlLZrddKZIZYthrsKz_3USUQRGvppCYZUSFxzp-lxel4-IH7ehl--_MHRnDGrs9BVtazkc8w9HMa27ahcVAbGv4h8vTSuCN1YJxjoZFB_DsdSfqZspdOhFnVMw.jpg",
     },
     {
       name: "Bitcoin Perth",
@@ -43,19 +43,19 @@ export const australiaResourcesEn: CountryResources = {
       name: "Bitcoin Adelaide",
       description: "Bitcoin meetup discussion and topics related to Bitcoin",
       url: "https://www.meetup.com/Adelaide-BTC/",
-      logo: "https://placehold.co/80x80?text=BA",
+      logo: "https://placehold.co/80x80?text=Adelaide",
     },
     {
       name: "Bitcoin Brisbane",
       description: "Join the official Bitcoin Brisbane meetup discussion group",
       url: "https://www.meetup.com/Bitcoin_Brisbane/",
-      logo: "https://placehold.co/80x80?text=BB",
+      logo: "https://cdn1.telesco.pe/file/naXKl7oQmVgjPocd0FGgpS82k0gily42SxBfEJ9rwtl_9YMmSmM2WkfUMoktOnnkraORYN3qT0V8nd3Rsc6zfRD-DGeje8WtCe-433FFc5s0oTe731ru2lVC_m9S3wJ36OQJzgr4vj4bIOqRnKiLKmcxcK7XYZLSTl7McXWKXGXZSwHp_3mdt7pHn7eDavuMoloU3ZG1d1_vm5OyQ9kRAgV1rLao-H1OKTrCYz-uZCzQArLZe2YV44jzGxLNuZW8kMsyikhkGuAMaLl1-VpWE0yvgZ9Yu4_6Vh-9E6WYZWJWl2OgfKm-Xs3T46JH42CuOtwRiNUBnpbVC3mfpn-ieg.jpg",
     },
     {
       name: "Bitcoin Hobart",
       description: "Tasmania's official Bitcoin meetup in Hobart",
       url: "https://www.meetup.com/bitcoin-southern-tassie-meetup-group/",
-      logo: "https://placehold.co/80x80?text=BH",
+      logo: "https://secure.meetupstatic.com/photos/event/6/f/e/5/clean_524548645.webp",
     },
   ],
   mining: [
@@ -64,7 +64,7 @@ export const australiaResourcesEn: CountryResources = {
       description:
         "Purchase a Bitaxe home miner through our store and start hashing!",
       url: "https://btcclub.com.au/product/bitaxe-601-gamma-bitcoin-solo-miner-bm1370-1-2-th-s/",
-      logo: "https://placehold.co/80x80?text=BTCCLUB",
+      logo: "https://pbs.twimg.com/profile_images/1796826697784057856/-E7D419N_400x400.jpg",
     },
   ],
   nodes: [
@@ -73,19 +73,19 @@ export const australiaResourcesEn: CountryResources = {
       description:
         "Let Parman know that BTCCLUB sent you! Australian owned!",
       url: "https://parmanode.com/",
-      logo: "https://placehold.co/80x80?text=Parmanode",
+      logo: "https://parmanode.com/images/pn_512.png",
     },
     {
       name: "Ministry of Nodes",
       description: "Take back your freedom with Bitcoin. Australian owned!",
       url: "https://www.ministryofnodes.com.au/",
-      logo: "https://placehold.co/80x80?text=MoN",
+      logo: "https://www.ministryofnodes.com.au/bl-content/uploads/Ministry%20of%20Nodes.png",
     },
     {
       name: "Start9",
       description: "Powerful Bitcoin Node. Use code BTCCLUB for 5% off",
       url: "https://store.start9.com/products/server-one",
-      logo: "https://placehold.co/80x80?text=Start9",
+      logo: "https://avatars.githubusercontent.com/u/53387992?s=200&v=4",
     },
   ],
   retail: [
@@ -94,14 +94,14 @@ export const australiaResourcesEn: CountryResources = {
       description:
         "From mining to T shirts and much more. They have you covered!",
       url: "https://shopbitcoin.com.au/",
-      logo: "https://placehold.co/80x80?text=SB",
+      logo: "https://pbs.twimg.com/profile_images/1814338070533259264/5mEcX5Pr_400x400.jpg",
     },
     {
       name: "The Bitcoin Diaries",
       description:
         "Fashion for the Financial Revolution. Bitcoin merch that converts",
       url: "https://thebitcoindiaries.com/",
-      logo: "https://placehold.co/80x80?text=TBD",
+      logo: "https://pbs.twimg.com/profile_images/1788685139863556096/v70Hn5wx_400x400.jpg",
     },
   ],
   charity: [
@@ -110,13 +110,13 @@ export const australiaResourcesEn: CountryResources = {
       description:
         "Australian Bitcoin Industry Body. Become a member today",
       url: "https://bitcoinindustrybody.org.au/",
-      logo: "https://placehold.co/80x80?text=ABIB",
+      logo: "https://pbs.twimg.com/profile_images/1431075888074919939/E4ZJf3ct_400x400.jpg",
     },
     {
       name: "Bitcoin Moon Fund",
       description: "Advancing Bitcoin in Australia. Donate now",
       url: "https://bitcoinmoonfund.org.au/",
-      logo: "https://placehold.co/80x80?text=BMF",
+      logo: "https://pbs.twimg.com/profile_images/1456797077669777411/TAjcstw0_400x400.jpg",
     },
   ],
 };
@@ -128,14 +128,14 @@ export const australiaResourcesKo: CountryResources = {
       description:
         "가장 큰 탈중앙화 비트코인 전용 컨퍼런스 - 무료입니다!",
       url: "https://bitcoinbushbash.info/",
-      logo: "https://placehold.co/80x80?text=BBB",
+      logo: "https://pbs.twimg.com/profile_images/1324113013301915649/1xyPdFF-_400x400.jpg",
     },
     {
       name: "Bitcoin Alive",
       description:
         "세계적인 연사들과 함께하는 가장 큰 비트코인 전용 컨퍼런스입니다",
       url: "https://bitcoinalive.io/",
-      logo: "https://placehold.co/80x80?text=BA",
+      logo: "https://pbs.twimg.com/profile_images/1594342404336885761/eBkrumv4_400x400.png",
     },
   ],
   meetups: [
@@ -144,14 +144,14 @@ export const australiaResourcesKo: CountryResources = {
       description:
         "비트코이너들이 운영하는, 신규 및 기존 비트코이너를 위한 밋업입니다",
       url: "https://www.meetup.com/melbourne-bitcoin-only/",
-      logo: "https://placehold.co/80x80?text=BM",
+      logo: "https://cdn5.telesco.pe/file/n6ek0cYGVHE0f-j27soQ4nUc-UiVoSNMLubWHUjY5UdXBzZJhT8tRweNkUidY8-ktoosKciVd1C-cZL2YOeDThyRmoA2TCnt06ptgoatvB85AukUjiCgispb5VynBSefPZYlP4S5tVYU0Fxk9gn6pRp06ntfW0emjdwKTIBaf8VU3UmY_KVBcDpAnpVZyiIBBdOrUiPXsxh2hQ2b7r2CgLBbwGrJ_nj29Q7c4KCt4QXRplMliGA9KKFuw_hmLEzHCbVzPyF9nrpJpPtJzLZ4FUYAXXJ6dyhFLFFwSyjJY5o-hud7nAx3bynmUqetCqY5W7YLh1bqD_fwRyxkYGnaFA.jpg",
     },
     {
       name: "Bitcoin Sydney",
       description:
         "유일하게 중요한 블록체인인 비트코인에 대한 토론 모임입니다",
       url: "https://www.meetup.com/Bitcoin_Sydney/",
-      logo: "https://placehold.co/80x80?text=BS",
+      logo: "https://cdn5.telesco.pe/file/grr-TtL_1OIOA1DfjIMlRtYyl9zuBP5yjne0T0lYOFzQ6uJeaUV_UMa4DBuXkvCKsdFvNwZatDkqEaDqUi8IwssF2fSMWPNiWNW4L5FF6XGumjMF5LKxE0zLXwSU28K1W0k5D9ZcpjAu2C3hfy7bfm8YlCAJqm2NwAPxOxolgPFzqO4IZ3-m3SmfBAj95wFFJ72PgxfjetgpQQJjFEHFaBVR5TJfhlLZrddKZIZYthrsKz_3USUQRGvppCYZUSFxzp-lxel4-IH7ehl--_MHRnDGrs9BVtazkc8w9HMa27ahcVAbGv4h8vTSuCN1YJxjoZFB_DsdSfqZspdOhFnVMw.jpg",
     },
     {
       name: "Bitcoin Perth",
@@ -164,19 +164,19 @@ export const australiaResourcesKo: CountryResources = {
       name: "Bitcoin Adelaide",
       description: "비트코인 관련 주제와 토론을 위한 밋업입니다",
       url: "https://www.meetup.com/Adelaide-BTC/",
-      logo: "https://placehold.co/80x80?text=BA",
+      logo: "https://placehold.co/80x80?text=Adelaide",
     },
     {
       name: "Bitcoin Brisbane",
       description: "공식 비트코인 브리즈번 밋업 토론 그룹에 참여하세요",
       url: "https://www.meetup.com/Bitcoin_Brisbane/",
-      logo: "https://placehold.co/80x80?text=BB",
+      logo: "https://cdn1.telesco.pe/file/naXKl7oQmVgjPocd0FGgpS82k0gily42SxBfEJ9rwtl_9YMmSmM2WkfUMoktOnnkraORYN3qT0V8nd3Rsc6zfRD-DGeje8WtCe-433FFc5s0oTe731ru2lVC_m9S3wJ36OQJzgr4vj4bIOqRnKiLKmcxcK7XYZLSTl7McXWKXGXZSwHp_3mdt7pHn7eDavuMoloU3ZG1d1_vm5OyQ9kRAgV1rLao-H1OKTrCYz-uZCzQArLZe2YV44jzGxLNuZW8kMsyikhkGuAMaLl1-VpWE0yvgZ9Yu4_6Vh-9E6WYZWJWl2OgfKm-Xs3T46JH42CuOtwRiNUBnpbVC3mfpn-ieg.jpg",
     },
     {
       name: "Bitcoin Hobart",
       description: "호바트에서 열리는 태즈메이니아 공식 비트코인 밋업입니다",
       url: "https://www.meetup.com/bitcoin-southern-tassie-meetup-group/",
-      logo: "https://placehold.co/80x80?text=BH",
+      logo: "https://secure.meetupstatic.com/photos/event/6/f/e/5/clean_524548645.webp",
     },
   ],
   mining: [
@@ -185,7 +185,7 @@ export const australiaResourcesKo: CountryResources = {
       description:
         "스토어에서 Bitaxe 홈 마이너를 구매하고 해싱을 시작하세요!",
       url: "https://btcclub.com.au/product/bitaxe-601-gamma-bitcoin-solo-miner-bm1370-1-2-th-s/",
-      logo: "https://placehold.co/80x80?text=BTCCLUB",
+      logo: "https://pbs.twimg.com/profile_images/1796826697784057856/-E7D419N_400x400.jpg",
     },
   ],
   nodes: [
@@ -194,19 +194,19 @@ export const australiaResourcesKo: CountryResources = {
       description:
         "BTCCLUB가 보냈다고 Parman에게 알려주세요! 호주 소유 기업입니다!",
       url: "https://parmanode.com/",
-      logo: "https://placehold.co/80x80?text=Parmanode",
+      logo: "https://parmanode.com/images/pn_512.png",
     },
     {
       name: "Ministry of Nodes",
       description: "비트코인으로 자유를 되찾으세요. 호주 소유 기업입니다!",
       url: "https://www.ministryofnodes.com.au/",
-      logo: "https://placehold.co/80x80?text=MoN",
+      logo: "https://www.ministryofnodes.com.au/bl-content/uploads/Ministry%20of%20Nodes.png",
     },
     {
       name: "Start9",
       description: "강력한 비트코인 노드. BTCCLUB 코드로 5% 할인받으세요",
       url: "https://store.start9.com/products/server-one",
-      logo: "https://placehold.co/80x80?text=Start9",
+      logo: "https://avatars.githubusercontent.com/u/53387992?s=200&v=4",
     },
   ],
   retail: [
@@ -215,14 +215,14 @@ export const australiaResourcesKo: CountryResources = {
       description:
         "마이닝부터 티셔츠까지 다양한 상품을 제공합니다!",
       url: "https://shopbitcoin.com.au/",
-      logo: "https://placehold.co/80x80?text=SB",
+      logo: "https://pbs.twimg.com/profile_images/1814338070533259264/5mEcX5Pr_400x400.jpg",
     },
     {
       name: "The Bitcoin Diaries",
       description:
         "금융 혁명을 위한 패션. 전환을 이끄는 비트코인 굿즈입니다",
       url: "https://thebitcoindiaries.com/",
-      logo: "https://placehold.co/80x80?text=TBD",
+      logo: "https://pbs.twimg.com/profile_images/1788685139863556096/v70Hn5wx_400x400.jpg",
     },
   ],
   charity: [
@@ -231,13 +231,13 @@ export const australiaResourcesKo: CountryResources = {
       description:
         "호주 비트코인 산업 협회. 오늘 회원이 되세요",
       url: "https://bitcoinindustrybody.org.au/",
-      logo: "https://placehold.co/80x80?text=ABIB",
+      logo: "https://pbs.twimg.com/profile_images/1431075888074919939/E4ZJf3ct_400x400.jpg",
     },
     {
       name: "Bitcoin Moon Fund",
       description: "호주에서 비트코인을 발전시킵니다. 지금 기부하세요",
       url: "https://bitcoinmoonfund.org.au/",
-      logo: "https://placehold.co/80x80?text=BMF",
+      logo: "https://pbs.twimg.com/profile_images/1456797077669777411/TAjcstw0_400x400.jpg",
     },
   ],
 };
